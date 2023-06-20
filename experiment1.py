9062384f7d2f39fa01cb2a3964fd24eeb242a456
Sebastian <kevin1111006@gmail.com>
2023-06-20 22:19:57 +0800

    final

--- experiment1.py
+++ experiment1.py
@@ -18,8 +18,9 @@ set2=np.zeros((100,20),dtype=int)
 minset=np.zeros(100,dtype=int)
 index=0
 numbers = [443, 552, 217, 319, 3, 708, 756, 342, 669, 133, 984, 405, 718, 105, 553, 66, 877, 705, 924, 654]
-for temperature in range(1000,101000,1000):
-    set1[index], set2[index] ,minset[index]= num.number_partitioning(numbers, initial_temperature=temperature, max_iterations=100000000)
+t = np.linspace(100,101000,100)
+for temperature in t:
+    set1[index], set2[index] ,minset[index]= num.number_partitioning(numbers, initial_temperature=temperature,cooling_rate=0.9999, max_iterations=100000000)
     # print(f"when T={temperature}:")
     # print("array=",numbers)
     # print("Set 1:", set1[index])
@@ -28,11 +29,10 @@ for temperature in range(1000,101000,1000):
     index+=1
 
 #plot_generate
-t = np.linspace(1000,101000,100)
 plt.plot(t,minset)
-plt.title('DiffInitemprature_plot')
+plt.title('DiffInitemprature_plot & coolingrate=0.9999')
 plt.xlabel('Initial temprature', fontsize=16)
 plt.ylabel('cost', fontsize=16)
 plt.grid()
-plt.savefig('DiffInitemprature_plot.png')
+plt.savefig('DiffInitemprature_plot & coolingrate=0.9999.png')
 plt.show()
--- experiment2.py
+++ experiment2.py
@@ -21,11 +21,11 @@ index=0
 numbers = [443, 552, 217, 319, 3, 708, 756, 342, 669, 133, 984, 405, 718, 105, 553, 66, 877, 705, 924, 654]
 for cooling in cool:
     set1[index], set2[index] ,minset[index]= num.number_partitioning(numbers, initial_temperature=62000,cooling_rate=cooling, max_iterations=100000000)
-    print(f"when cooling_rate={cooling}:")
-    print("array=",numbers)
-    print("Set 1:", set1[index])
-    print("Set 2:", set2[index])
-    print("Cost:", minset[index],"\n")
+    # print(f"when cooling_rate={cooling}:")
+    # print("array=",numbers)
+    # print("Set 1:", set1[index])
+    # print("Set 2:", set2[index])
+    # print("Cost:", minset[index],"\n")
     index+=1
 
 #plot_generate
